44809adf1b739d1dd341605ec3cb29bc45feae0c
yungcheeze <ucizim@gmail.com>
2018-11-14 11:20:50 +0000

    Add cmake-ide dir-locals setup

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil .
+      ((cmake-ide-project-dir . "~/<Project>/")
+       (cmake-ide-build-dir . "~/<Project>/cmake-ide-build")
+       (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=Debug")
+       (helm-make-build-dir . "cmake-ide-build")
+       (helm-make-arguments . "-j4")
+       (helm-ctest-dir . "~/<Project>/build/cmake-ide-build")
+       )))
